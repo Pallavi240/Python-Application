18db8545f9b902ab96c94c98361b1783d88c6ce1
Pallavimahapriya <pallavi.mahapriya@srmtech.com>
2025-10-08 12:35:20 +0530

    Speling mistake

--- firebase-auth-app/src/router/index.js
+++ firebase-auth-app/src/router/index.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginPage from '../pages/LoginPage.vue'
+import Loginpage from '../pages/Loginpage.vue'
 import SignupPage from '../pages/SignupPage.vue'
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: LoginPage },
-  { path: '/signup', component: SignupPage }
+  { path: '/login', component: Loginpage },
+  { path: '/signup', component: SignupPage },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 })
 
 export default router
